d6ecedb3fc91a40df0aa2dd930d767fd290840cd
Randhir Kumar <1209randhir@gmail.com>
2025-02-01 21:43:57 +0530

    Add files via upload

---
+++ patnum2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int row,col,n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for (row=1;row<=n;row++){
+        for(col=1;col<=n;col++){
+            printf(" %d ",col);
+        }
+        printf("\n");
+    }
+    return 0;
+}
